be36433a6e446f742a1371de137a3de569b01d91
MRo47 <rodriguesmyron47@gmail.com>
2024-07-15 19:58:39 +0100

    use docker compose

--- ros2_dev_container/export_env.sh
+++ ros2_dev_container/export_env.sh
@@ -6,3 +6,4 @@ echo "USER_NAME=${USER}" > ${ENV_FILE}
 echo "USER_ID=$(id -u $USER)" >> ${ENV_FILE}
 echo "GROUP_NAME=$(id -gn $USER)" >> ${ENV_FILE}
 echo "GROUP_ID=$(id -g $USER)" >> ${ENV_FILE}
+echo "DISPLAY=${DISPLAY}" >> ${ENV_FILE}
